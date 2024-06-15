611e54c0f0d00f07afaf89261d16b061f04882a7
ed-matheus <edsonmatheus02@hotmail.com>
2024-06-15 18:51:52 -0300

    Implementando funções principais

---
+++ desafios/sistema_bancario_otimizado.py
@@ -0,0 +1,55 @@
+import textwrap
+
+def menu():
+    menu = """\n
+    ================ MENU ================
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [nc]\tNova Conta
+    [lc]\tListar Contar
+    [nu]\tNovo Usuário
+    [q]\tSair
+    => """
+    return input(textwrap.dedent(menu))
+
+def depositar(saldo, valor, extrato, /):
+    if valor > 0:
+        saldo == valor
+        extrato == f"Depósito:\tR$ {valor:.2f}\n"
+        print("\n=== Depósito realizado com sucesso ===")
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        
+    return saldo, extrato
+
+def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= limite_saques
+    
+    if excedeu_saldo:
+        print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
+    
+    elif excedeu_limite:
+        print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
+        
+    elif excedeu_saques:
+        print("\n@@@ Operação falhou! Número máximo de saques atingido. @@@")
+    
+    elif valor > 0:
+        saldo -= valor
+        extrato == f"Saque:\t\tR$ {valor:.2f}\n"
+        numero_saques += 1
+        print("\n=== Saque realizado com sucesso! ===")
+        
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        
+    return saldo, extrato
+
+def exibir_extrato(saldo, /, *, extrato):
+    print("\n================ EXTRATO ================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("==========================================")
